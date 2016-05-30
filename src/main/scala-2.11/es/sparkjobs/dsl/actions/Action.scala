bbbafec90c353aaddfc2ee5e58d76b84d97790b3
Ruben J Garcia <rubenjgarciab@gmail.com>
2016-05-30 15:42:56 +0200

    Added new actions

--- src/main/scala-2.11/es/sparkjobs/dsl/actions/Action.scala
+++ src/main/scala-2.11/es/sparkjobs/dsl/actions/Action.scala
@@ -2,10 +2,31 @@ package es.sparkjobs.dsl.actions
 
 import org.apache.spark.rdd.RDD
 
+//  val reduce = 100
+//  val takeSample = 105
+//  val takeOrdered = 106
+//  val saveAsTextFile = 107
+//  val saveAsSequenceFile = 108
+//  val saveAsObjectFile = 109
+//  val countByKey = 110
+//  val foreach = 111
+
 trait Action[T] {
   def apply(rdd: RDD[_]) : T
 }
 
 case class CollectAction() extends Action[Array[_]] {
   override def apply(rdd: RDD[_]) = rdd.collect()
+}
+
+case class CountAction() extends Action[Long] {
+  override def apply(rdd: RDD[_]) = rdd.count()
+}
+
+case class FirstAction() extends Action[Any] {
+  override def apply(rdd: RDD[_]) = rdd.first()
+}
+
+case class TakeAction(n: Int) extends Action[Array[_]] {
+  override def apply(rdd: RDD[_]) = rdd.take(n)
 }
--- src/main/scala-2.11/es/sparkjobs/dsl/actions/package.scala
+++ src/main/scala-2.11/es/sparkjobs/dsl/actions/package.scala
@@ -2,4 +2,7 @@ package es.sparkjobs.dsl
 
 package object actions {
   implicit def collect = CollectAction()
+  implicit def count = CountAction()
+  implicit def first = FirstAction()
+  implicit def take(n: Int) = TakeAction(n)
 }
