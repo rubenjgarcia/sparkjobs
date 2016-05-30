601373365f67b5458b16d61bead9978a81a45813
Ruben J Garcia <rubenjgarciab@gmail.com>
2016-05-30 15:51:46 +0200

    Path to resources file

--- src/test/scala-2.11/es/sparkjobs/test/WordCountSpec.scala
+++ src/test/scala-2.11/es/sparkjobs/test/WordCountSpec.scala
@@ -4,14 +4,17 @@ import es.sparkjobs.dsl.SparkJobDSL._
 import es.sparkjobs.dsl.rdds._
 import es.sparkjobs.dsl.transformations._
 import es.sparkjobs.dsl.actions._
+import scala.io.Source
 import org.scalatest._
 
 class WordCountSpec extends UnitSpec {
 
+  val source = getClass.getResource("/")
+
   "It" should "count words" in {
     val job = {
       config("Word Count", "local[8]") rdd {
-        fromfile("/home/rjgarcia/Documents/Projects/SparkJobs/target/scala-2.11/classes/../../../src/main/resources/quijote.txt") transformWith {
+        fromfile(source + "quijote.txt") transformWith {
           split(" ") andWith
             filter(3) andWith
             replaceAll(Array(",", ":", ";", "[?]", "¿", "!", "¡", "\"", "[(]", "[)]", "[.]"), "") andWith
